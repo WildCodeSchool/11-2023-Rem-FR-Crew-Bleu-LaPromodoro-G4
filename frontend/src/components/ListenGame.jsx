5a11638bd2bad2f8cb2907525a4ce16ce6cdfed6
Lucien KOHLI <lucien.kohli@gmail.com>
2024-01-11 14:38:52 +0100

    ListenGame fonctionnel

---
+++ frontend/src/components/ListenGame.jsx
@@ -0,0 +1,138 @@
+import { useState } from "react";
+import "../style/ListenGame.css";
+import Speech from "react-text-to-speech";
+// import logo from "../assets/Group_1.png";
+
+function ListenGame() {
+  const Words = [
+    { id: "1", word: "Poulet" },
+    { id: "2", word: "Nature" },
+    { id: "3", word: "Armoire" },
+    { id: "4", word: "Rembarde" },
+    { id: "5", word: "Chaloupe" },
+    { id: "6", word: "Hareng" },
+    { id: "7", word: "Grenouille" },
+    { id: "8", word: "Rododindron" },
+    { id: "9", word: "Tilleul" },
+    { id: "10", word: "chrysanthème" },
+  ];
+
+  // BOUTON POUR LANCER LE TEXT TO SPEECJ
+
+  const startBtn = (
+    <button className="my-start-btn" type="button">
+      Cliquez pour écouter
+    </button>
+  );
+
+  // lISTES DES STATES
+
+  // state actualisant le mot du tableau
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  // state actualisant l'input que rentre l'utilisateur pour épeler le mot du speech
+  const [userInput, setUserInput] = useState("");
+  // state actualisant le score selon les réponses de l'utilisateur dans l'input
+  const [userScore, setUserScore] = useState(0);
+  // state qui détermine si le quiz est arrivé au bout des mots du tableau
+  const [quizFinished, setQuizFinished] = useState(false);
+  // state initialisée comme tableau où seront stockée les réponses de l'utilisateur (remplit de null qui change aux rentrées d'input du user)
+  const [userAnswers, setUserAnswers] = useState(
+    Array(Words.length).fill(null)
+  );
+
+  // Fonction appelée lors du passage au mot suivant
+  const handleNextWord = () => {
+    const correctWord = Words[currentWordIndex].word.toUpperCase();
+    const isCorrect = userInput === correctWord;
+
+    // Mise à jour des réponses de l'utilisateur
+    setUserAnswers((prevAnswers) => {
+      const newAnswers = [...prevAnswers];
+      newAnswers[currentWordIndex] = isCorrect;
+      return newAnswers;
+    });
+
+    // Mise à jour du score de l'utilisateur
+    setUserScore((prevScore) => prevScore + (isCorrect ? 1 : 0));
+
+    // Passage au mot suivant ou fin du quiz
+    setCurrentWordIndex((prevIndex) => {
+      if (prevIndex < Words.length - 1) {
+        setUserInput("");
+        return prevIndex + 1;
+      }
+      setQuizFinished(true);
+      return prevIndex;
+    });
+  };
+
+  // Passage de la saisie du user en majuscule
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value.toUpperCase());
+  };
+
+  // fonction pour que la touche entrée provoque la fonction handleNextWord
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleNextWord();
+    }
+  };
+
+  return (
+    <>
+      {/* <img className="logo1" src={logo} alt="" /> */}
+      <div className="header">
+        <div className="leave">Quitter</div>
+        <p className="level">Niveau 1: Écoute</p>
+      </div>
+      <div className="game">
+        <div className="quizSpace">
+          <Speech
+            className="speech"
+            text={Words[currentWordIndex].word}
+            pitch={1.5}
+            rate={2}
+            volume={0.3}
+            startBtn={startBtn}
+          />
+          <input
+            className="wordType"
+            type="text"
+            value={userInput}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+          <div
+            tabIndex={0}
+            role="button"
+            className={userInput !== "" ? "my-next-btn" : "no-next-btn"}
+            onClick={handleNextWord}
+            onKeyDown={handleNextWord}
+          >
+            Suivant
+          </div>
+        </div>
+        {/* Barre de progression, si les user answers sont true active classe true sinon false */}
+        <div className="progressBar">
+          {Words.map((word, index) => {
+            let className = "";
+
+            if (userAnswers[index] !== null) {
+              className = userAnswers[index] ? "true" : "false";
+            }
+
+            return <div key={word.id} className={className} />;
+          })}
+        </div>
+        <div className="score">{`${userScore}/10`}</div>
+      </div>
+      {quizFinished && (
+        <div className="end">
+          <p>Quiz finished! Well played!</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ListenGame;
