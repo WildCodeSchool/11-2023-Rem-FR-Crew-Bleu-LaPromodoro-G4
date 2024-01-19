b1c1db816502e71d606ed58779dbe844fc7b178d
Lucien KOHLI <lucien.kohli@gmail.com>
2024-01-19 10:42:09 +0100

    fix Result quizz

--- frontend/src/pages/Quizz.jsx
+++ frontend/src/pages/Quizz.jsx
@@ -199,10 +199,10 @@ function Quizz() {
           <p className="levelQuizz">Niveau 3: Jeu de définitions</p>
         </div>
       )}
-      {/* <div className="scoreQuizz">
+      <div className="scoreQuizz">
         <div>Score:</div>
         <div id="score">0</div>
-      </div> */}
+      </div>
       <h3 className="instruction">
         Lis la question et choisis la bonne réponse:
       </h3>
