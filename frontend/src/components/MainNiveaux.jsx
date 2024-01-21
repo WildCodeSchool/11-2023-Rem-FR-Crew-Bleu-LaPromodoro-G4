a005e52d43e88bf6acb646911192b6389ae101a0
Goodtimenl <gregory.delaloeuf@gmail.com>
2024-01-21 11:39:57 +0100

    fixed last bugs and comment code

--- frontend/src/components/MainNiveaux.jsx
+++ frontend/src/components/MainNiveaux.jsx
@@ -3,24 +3,28 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function MainNiveaux() {
-  // Récupération du thème de l'utilisateur depuis le localStorage
+  // Récupère et parse le thème utilisateur depuis le localStorage.
+  // Si non trouvé, définit un objet vide comme valeur par défaut
   const userTheme = JSON.parse(localStorage.getItem("userTheme")) || "";
 
-  // Récupération et mise en place du score de l'utilisateur
+  // Récupère le score utilisateur depuis le localStorage
+  // convertion en entier, avec une valeur par défaut de 0 si y'a rien dedans
   const userScoreFromLocalStorage =
     parseInt(localStorage.getItem("totalScore"), 10) || 0;
+  // Utilise useState pour créer 'userScore' avec la valeur récupérée
   const [userScore, setUserScore] = useState(userScoreFromLocalStorage);
 
-  // Mise à jour du score si changé dans le localStorage
+  // useEffect pour écouter les changements dans le localStorage
   useEffect(() => {
+    // Définit une fonction pour gérer les changements du localStorage
     const handleStorageChange = () => {
+      // Met à jour le score utilisateur en le récupérant à nouveau du localStorage
       const updatedScore =
         parseInt(localStorage.getItem("totalScore"), 10) || 0;
-      setUserScore(updatedScore);
+      setUserScore(updatedScore); // Met à jour l'état 'userScore'
     };
 
     window.addEventListener("storage", handleStorageChange);
-
     return () => {
       window.removeEventListener("storage", handleStorageChange);
     };
@@ -28,12 +32,16 @@ function MainNiveaux() {
 
   return (
     <div className="container">
+      {/* Bloc pour le Niveau 1 */}
       <div className="niveau1">
         <h2>Niveau 1: Écoute</h2>
+        {/* Le lien change en fonction du score utilisateur :
+            - Dirige vers "/ListenGameBis" si score >= 10 Sinon, dirige vers "/speech" */}
         <Link to={userScore >= 10 ? "/ListenGameBis" : "/speech"}>
           <button
             type="button"
             className="btnEnter1"
+            // Style du bouton basé sur le thème utilisateur.
             style={{
               color: userTheme.color,
               backgroundColor: userTheme.backgroundColor,
@@ -50,8 +58,12 @@ function MainNiveaux() {
         />
       </div>
 
+      {/* Bloc pour le Niveau 2 */}
       <div className="niveau2">
         <h2>Niveau 2: Synonymes</h2>
+        {/* - Si score >= 10, affiche un lien fonctionnel.
+            - Si score >= 20, le lien dirige vers "/WordSynonymComponentBis".
+            - Si score < 10, affiche un bouton désactivé avec un style différent. */}
         {userScore >= 10 ? (
           <Link to={userScore >= 20 ? "/WordSynonymComponentBis" : "/synonym"}>
             <button
@@ -87,8 +99,11 @@ function MainNiveaux() {
         />
       </div>
 
+      {/* Bloc pour le Niveau 3 */}
       <div className="niveau3">
         <h2>Niveau 3: Quizz</h2>
+        {/* - Si score >= 20, affiche un lien fonctionnel vers "/quizz".
+            - Si score < 20, affiche un bouton désactivé avec un style différent. */}
         {userScore >= 20 ? (
           <Link to="/quizz">
             <button
@@ -117,6 +132,7 @@ function MainNiveaux() {
             Verrouillé
           </button>
         )}
+        {/* Commenté : Icône de cadenas pour le niveau */}
         {/* <img
           src="/src/assets/cadenas.png"
           alt="icone cadenas"
