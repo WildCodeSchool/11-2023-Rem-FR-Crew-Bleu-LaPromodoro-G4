dc7ee0bc2b3704538f00ae3c7bf0fb04c40b53a0
Goodtimenl <gregory.delaloeuf@gmail.com>
2024-01-23 11:08:52 +0100

    add link to level 4 with conditions

--- frontend/src/components/MainNiveaux.jsx
+++ frontend/src/components/MainNiveaux.jsx
@@ -105,7 +105,7 @@ function MainNiveaux() {
         {/* - Si score >= 20, affiche un lien fonctionnel vers "/quizz".
             - Si score < 20, affiche un bouton désactivé avec un style différent. */}
         {userScore >= 20 ? (
-          <Link to="/quizz">
+          <Link to={userScore >= 30 ? "/quizzBis" : "/quizz"}>
             <button
               type="button"
               className="btnEnter3"
@@ -139,6 +139,48 @@ function MainNiveaux() {
           className="lock3"
         /> */}
       </div>
+      {/* Bloc pour le Niveau 4 */}
+      <div className="niveau4">
+        <h2>Niveau 4: Mélange des mots</h2>
+        {/* - Si score >= 10, affiche un lien fonctionnel.
+            - Si score >= 20, le lien dirige vers "/WordSynonymComponentBis".
+            - Si score < 10, affiche un bouton désactivé avec un style différent. */}
+        {userScore >= 30 ? (
+          <Link to={userScore >= 40 ? "/LienBis" : "/lien"}>
+            <button
+              type="button"
+              className="btnEnter2"
+              style={{
+                color: userTheme.color,
+                backgroundColor: userTheme.backgroundColor,
+                cursor: userTheme.crs,
+              }}
+            >
+              Entrer
+            </button>
+          </Link>
+        ) : (
+          <button
+            type="button"
+            className="btnEnter2"
+            disabled
+            style={{
+              color: userTheme.color,
+              backgroundColor: "#cccccc",
+              cursor: "not-allowed",
+            }}
+          >
+            Verrouillé
+          </button>
+        )}
+        <img
+          src="/src/assets/cadenas.png"
+          alt="icone cadenas"
+          className="lock2"
+        />
+      </div>
+
+      {/* Bloc pour le Niveau 3 */}
     </div>
   );
 }
