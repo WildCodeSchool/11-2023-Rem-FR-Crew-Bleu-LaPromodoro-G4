ccd8d462653a94629f0b1f5475f6e93872aa6ed1
ERIC BESSON <e-besson@laposte.net>
2024-01-24 20:27:04 +0100

    ok component

--- frontend/src/components/EndComponent.jsx
+++ frontend/src/components/EndComponent.jsx
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../style/EndComponent.css";
 import { Link } from "react-router-dom";
 
 function EndComponent() {
+  const userNameFromLocalStorage = localStorage.getItem("userName");
+  const [imagePath, setImagePath] = useState("");
+
+  useEffect(() => {
+    const userThemeFromLocalStorage = localStorage.getItem("userTheme");
+
+    if (userThemeFromLocalStorage) {
+      const userThemeObject = JSON.parse(userThemeFromLocalStorage);
+      const path = userThemeObject.preview;
+
+      setImagePath(path);
+    }
+  }, []);
+
   const clearLocalStorage = () => {
     localStorage.clear();
   };
 
   return (
     <div className="end-component">
+      <img className="picture" src={imagePath} alt="" />
       <h1>
-        Félicitation <br /> Tu as atteint les 40 points !{" "}
+        Félicitations {userNameFromLocalStorage} <br /> Tu as atteint les 40
+        points !{" "}
       </h1>
 
-      <Link to="/">
+      <Link to="/onboarding">
         <button
           type="button"
           className="btn-restart"
